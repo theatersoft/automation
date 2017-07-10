e51611bee836a9a5f4b7634597f5b7cb305626c5
Rob Huehn <rhuehn@gmail.com>
2017-07-10 12:50:05 -0700

    tidy

--- src/tasks/sensorAlerts.js
+++ src/tasks/sensorAlerts.js
@@ -3,23 +3,22 @@ import {Type} from '@theatersoft/device'
 import {setFeed} from '../actions'
 import {bus, proxy} from '@theatersoft/bus'
 
-const select = getState => ({Device: {devices}} = getState()) => devices
-
-const prev = (getState, _prev = {}) => f => (_state = getState()) => {
-    f(_state, _prev)
-    _prev = _state
-}
-
-const diffValues = (a, b) => b && a.value !== b.value
-
-const diffs = (state, prev) => Object.entries(state).reduce((o, [k,v]) => {
-    if (typeof v.value !== 'object' && diffValues(v, prev[k])) o[k] = v
-    return o
-}, {})
+const
+    select = getState => ({Device: {devices}} = getState()) => devices,
+    prev = (getState, _prev = {}) => f => (_state = getState()) => {
+        f(_state, _prev)
+        _prev = _state
+    },
+    diffValues = (a, b) => b && a.value !== b.value,
+    diffs = (state, prev) => Object.entries(state).reduce((o, [k,v]) => {
+        if (typeof v.value !== 'object' && diffValues(v, prev[k])) o[k] = v
+        return o
+    }, {}),
+    _time = t => new Date(t).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric'}).toLowerCase()
 
 export class SensorAlerts {
-    start ({store}) {
-        store.subscribe(prev(select(store.getState))((state, prevState) => Object.values(diffs(state, prevState))
+    start ({store: {subscribe, getState, dispatch}}) {
+        subscribe(prev(select(getState))((state, prevState) => Object.values(diffs(state, prevState))
             .forEach(device => {
                 const {name, type, value, id, time} = device
                 let status
@@ -32,13 +31,9 @@ export class SensorAlerts {
                     break
                 }
                 if (status) process.nextTick(() => {
-                    store.dispatch(setFeed({severity: value ? 1 : 2, id, type, name, status, time}))
-                    if (store.getState().settings.armed) proxy('Session').sendPush(JSON.stringify({
-                        body: `${name} ${status} at ${new Date(time).toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric'}).toLowerCase()}`,
-                        icon: '/res/theatersoft-logo-round-accent.png',
-                        tag: id,
-                        renotify: false
-                    }))
+                    dispatch(setFeed({severity: value ? 1 : 2, id, type, name, status, time}))
+                    if (getState().settings.armed)
+                        proxy('Session').sendPush(JSON.stringify({body: `${name} ${status} at ${_time(time)}`, icon: '/res/theatersoft-logo-round-accent.png', tag: id, renotify: false}))
                     else
                         log(`Not armed, not sending ${name} ${status}`)
                 })
