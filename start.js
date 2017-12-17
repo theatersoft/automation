aac8d20950ffdf454eb2cc044999ad3eeafb6b13
Rob Huehn <rhuehn@gmail.com>
2017-12-16 20:13:11 -0800

    convert to startLocalService

--- start.js
+++ start.js
@@ -1,20 +1,9 @@
 'use strict'
-require('@theatersoft/bus').setTime(true)
-require('@theatersoft/bus').setTag('Automation')
-require('@theatersoft/bus').bus.start()
-    .then(() => {
-        const
-            options = {
-                module: '@theatersoft/automation',
-                export: 'Automation',
-                name: 'Automation',
-                config: {
-                    remotedev: 'localhost'
-                }
-            },
-            service = new (require(options.module)[options.export])()
-        console.log('starting', options)
-        service.start(options)
-        process.on('SIGINT', () =>
-            service.stop().then(() => process.exit()))
-    })
+require('@theatersoft/server/lib').startLocalService({
+    module: '@theatersoft/automation',
+    export: 'Automation',
+    name: 'Automation',
+    config: {
+        remotedev: 'localhost'
+    }
+})
