5ef9e73204a90030a7d0e8303dbf5dfee904debe
Rob Huehn <rhuehn@gmail.com>
2018-04-09 08:30:23 -0700

    enable single task start in own service

--- start.js
+++ start.js
@@ -1,1 +1,10 @@
-require('@theatersoft/server/lib').startLocalService('Automation')
+const
+    {startLocalService} = require('@theatersoft/server/lib'),
+    task = process.argv[2],
+    service = task ? {
+        module: '@theatersoft/automation',
+        export: 'Automation',
+        name: `Automation.${task}`,
+        config: {[task]: true}
+    } : 'Automation'
+startLocalService(service)
