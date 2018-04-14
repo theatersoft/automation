a60723f0f6f44eb9de875b83f42a46e0187ee337
Rob Huehn <rhuehn@gmail.com>
2018-04-14 12:13:47 -0700

    strip debug logs from normal build

--- build.js
+++ build.js
@@ -6,6 +6,7 @@ const
     pkg = require('./package.json'),
     name = pkg.name.startsWith('@theatersoft') && pkg.name.slice(13),
     DIST = process.env.DIST === 'true',
+    DEBUG = process.env.DEBUG === 'true',
     deleteKey = (o, k) => (k && delete o[k], o),
     dependencies = deleteKey(pkg.dist.dependencies, DIST && 'remote-redux-devtools'),
     path = require('path'),
@@ -16,13 +17,15 @@ const
     babel = require('rollup-plugin-babel'),
     ignore = require('rollup-plugin-ignore'),
     nodeResolve = require('rollup-plugin-node-resolve'),
-    strip = require('rollup-plugin-strip'),
     sourcemaps = require('rollup-plugin-sourcemaps'),
     postcss = require('rollup-plugin-postcss'),
     stylus = require('stylus'),
     postcssModules = require('postcss-modules'),
     cssExportMap = {},
-    alias = require('rollup-plugin-alias')
+    alias = require('rollup-plugin-alias'),
+    strip = !DEBUG && require('rollup-plugin-strip')({
+        functions: DIST ? ['log', 'debug', 'composeWithDevTools'] : ['debug']
+    })
 
 const targets = {
     node () {
@@ -65,9 +68,7 @@ const targets = {
                     ] : [])
                 }),
                 DIST && ignore(['remote-redux-devtools']),
-                DIST && strip({
-                    functions: ['composeWithDevTools']
-                }),
+                strip,
                 alias({'@theatersoft/automation': 'src/index.js'}),
                 nodeResolve({jsnext: true})
             ]
